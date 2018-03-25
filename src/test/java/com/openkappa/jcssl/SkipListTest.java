dae2a3ed8d23a428992cf20f3cc5efe5cfd120c0
Richard Startin <richard@openkappa.co.uk>
2018-03-25 16:34:17 +0100

    add some more tests

--- src/test/java/com/openkappa/jcssl/SkipListTest.java
+++ src/test/java/com/openkappa/jcssl/SkipListTest.java
@@ -18,4 +18,32 @@ public class SkipListTest {
     assertFalse(sl.contains(3));
   }
 
+
+  @Test
+  public void createSkipListMissingValue() {
+    SkipList sl = SkipList.createSkipList((byte)2, (byte)256);
+    sl.insert(0);
+    sl.insert(1);
+    sl.insert(3);
+    assertTrue(sl.contains(0));
+    assertTrue(sl.contains(1));
+    assertTrue(sl.contains(3));
+    assertFalse(sl.contains(2));
+  }
+
+
+  @Test
+  public void createSkipListSparse() {
+    SkipList sl = SkipList.createSkipList((byte)2, (byte)4);
+    sl.insert(0);
+    sl.insert(10000);
+    sl.insert(20000);
+    assertTrue(sl.contains(0));
+    assertTrue(sl.contains(10000));
+    assertTrue(sl.contains(20000));
+    assertFalse(sl.contains(3));
+  }
+
+
+
 }
